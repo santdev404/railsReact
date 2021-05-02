692f604fc83d4521b74c990b55453f6340924ddf
santdev404 <santdev404@gmail.com>
2021-05-01 20:45:48 -0500

    First commit

---
+++ db/seeds.rb
@@ -0,0 +1,44 @@
+airlines = Airline.create([
+  { 
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  }, 
+  { 
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+  }, 
+  { 
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+  }, 
+  { 
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+  }
+])
+      
+
+
+
+reviews = Review.create([
+    {
+        title: 'Great airline',
+        description: 'I had a lovely time',
+        score: 5,
+        airline: airlines.first
+    },
+    {
+        title: 'Bad airline',
+        description: 'I had a bad time',
+        score: 1,
+        airline: airlines.first
+    },
+])
